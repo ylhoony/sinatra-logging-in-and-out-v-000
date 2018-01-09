ffc1a66d229179c11fe2624908bb48d5fc75b980
Hoon Lee <yl.hoony@gmail.com>
2018-01-09 08:40:55 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,6 +1,6 @@
 class Helpers
   def self.current_user(session_hash)
-    User.find_by_id(session_hash[:user_id])
+    @user = User.find_by_id(session_hash[:user_id])
   end
 
   def self.is_logged_in?(session_hash)
